ec43c7f9072728aa83dca375f91fe1ced700c06a
aqib <maqibzaved@gmail.com>
2017-04-15 15:09:08 +0600

    service db and controller

---
+++ application/controllers/Service.php
@@ -0,0 +1,216 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Service extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Loginm');
+        $this->load->model('Menum');
+        $this->load->model('Aboutusm');
+    }
+
+    public function index()
+    {
+        $this->load->view('service');
+    }
+
+    public function about_us_page()
+    {
+        if ($this->session->userdata('type') == "Admin") {
+
+//            $id = $this->session->userdata('id');
+//            $this->data['user_info'] = $this->Loginm->get_user($id);
+
+            $this->data['about_us'] = $this->Aboutusm->get_all_about_us();
+            $this->data['aboutus'] = $this->Aboutusm->get_aboutus();
+            $this->data['about_details'] = $this->Aboutusm->get_aboutus_details();
+            $this->data['about_banner'] = $this->Aboutusm->get_aboutus_banner();
+            $this->load->view('about_us_for_admin', $this->data);
+        } else {
+            redirect('Home');
+        }
+    }
+
+    public function add_new_about_head()
+    {
+        if ($this->session->userdata('type') == "Admin") {
+
+//            $id = $this->session->userdata('id');
+//            $this->data['user_info'] = $this->Loginm->get_user($id);
+
+
+            $this->load->view('add_new_about_head');
+        } else {
+            redirect('Home');
+        }
+    }
+
+    public function add_new_about_us()
+    {
+        if ($this->session->userdata('type') == "Admin") {
+
+//            $id = $this->session->userdata('id');
+//            $this->data['user_info'] = $this->Loginm->get_user($id);
+
+
+            //$this->load->view('add_new_about_head');
+
+
+            $about_us_head=$this->input->post('about_us_head');
+            $about_us_details=$this->input->post('about_us_details');
+            $about_us_quote=$this->input->post('about_us_quote');
+            $this->data['add'] = $this->Aboutusm->add_about_us($about_us_head,$about_us_details,$about_us_quote);
+            redirect('About_us/about_us_page');
+
+
+        } else {
+            redirect('Home');
+        }
+    }
+
+
+    public function edit_about_us()
+    {
+        if ($this->session->userdata('type') == "Admin") {
+
+            $id = $this->session->userdata('id');
+            $this->data['name']= $this->Loginm->get_username($id);
+
+//            $this->data['about_us'] = $this->Aboutusm->get_all_about_us();
+//            $this->load->view('about_us_for_admin', $this->data);
+
+            $id1=$this->input->post('about_us_id');
+            $about_us_head=$this->input->post('about_us_head');
+            $about_us_details=$this->input->post('about_us_details');
+            $about_us_quote=$this->input->post('about_us_quote');
+
+//            $image=  $_FILES["Photo"]["name"];
+//            move_uploaded_file($_FILES["file"]["tmp_name"], "images/" . $image);
+
+            $this->data['edit'] = $this->Aboutusm->edit_about_us($id1,$about_us_head,$about_us_details,$about_us_quote);
+            redirect('About_us/about_us_page');
+
+
+
+        } else {
+            redirect('Home');
+        }
+    }
+
+    public function update_about_us($id)
+    {
+        if ($this->session->userdata('type') == "Admin") {
+
+            $this->Aboutusm->update_aboutus_banner($id);
+            redirect('About_us/about_us_page');
+
+
+        } else {
+            redirect('Home');
+        }
+    }
+
+    public function show_edit_about_us()
+    {
+        if ($this->session->userdata('type') == "Admin") {
+
+            $id = $this->session->userdata('id');
+            $this->data['user_info'] = $this->Loginm->get_user($id);
+
+//            $this->data['about_us'] = $this->Aboutusm->get_all_about_us();
+//            $this->load->view('about_us_for_admin', $this->data);
+            $id1=$this->input->post('id');
+            $about_us_head=$this->input->post('about_us_head');
+            $about_us_details=$this->input->post('about_us_details');
+            $about_us_quote=$this->input->post('about_us_quote');
+            $this->data['about_banner'] = $this->Aboutusm->get_edit_aboutus_banner($id1);
+            $this->load->view('edit_admin_about_us',$this->data);
+
+        } else {
+            redirect('Home');
+        }
+    }
+
+    public function show_edit_about_us_details()
+    {
+        if ($this->session->userdata('type') == "Admin") {
+
+            $id=$this->input->post('id');
+            $this->data['about_details'] = $this->Aboutusm->get_edit_aboutus_details($id);
+            $this->load->view('edit_admin_about_us_details',$this->data);
+
+        } else {
+            redirect('Home');
+        }
+    }
+
+    public function update_about_us_details($id)
+    {
+        if ($this->session->userdata('type') == "Admin") {
+
+            $this->Aboutusm->update_aboutus_details($id);
+            redirect('About_us/about_us_page');
+
+
+        } else {
+            redirect('Home');
+        }
+    }
+
+    public function view_add_new_banner()
+    {
+
+
+        if ($this->session->userdata('type') == "Admin") {
+
+            $this->load->view('add_admin_about_us_banner');
+
+        }
+        else{
+            redirect('Home');
+        }
+    }
+
+    public function view_add_new_details()
+    {
+
+
+        if ($this->session->userdata('type') == "Admin") {
+
+            $this->load->view('add_admin_about_us_details');
+
+        }
+        else{
+            redirect('Home');
+        }
+    }
+
+    public function add_new_banner()
+    {
+
+
+        if ($this->session->userdata('type') == "Admin") {
+            $this->Aboutusm->add_new_banner();
+            redirect('About_us/about_us_page');
+        }
+        else{
+            redirect('Home');
+        }
+    }
+
+    public function add_new_details()
+    {
+
+
+        if ($this->session->userdata('type') == "Admin") {
+            $this->Aboutusm->add_new_details();
+            redirect('About_us/about_us_page');
+        }
+        else{
+            redirect('Home');
+        }
+    }
+}
---
+++ application/views/service.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>Website Design & Development | A2N</title>
+		<meta name="description" content="Responsive Multi-Level Menu: Space-saving drop-down menu with subtle effects" />
+		<meta name="keywords" content="multi-level menu, mobile menu, responsive, space-saving, drop-down menu, css, jquery" />
+		<meta name="author" content="Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico"> 
+        
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+        
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="js/modernizr.custom.js"></script>
+	</head>
+	<body>
+		<div class="container-fluid demo-4" style="background:url(images/about-us-bg.jpg) no-repeat center center; background-size:cover">
+        	<div class="container">
+            	<div class="row" >            	
+                		
+				<div class="col-md-3">
+					<div id="dl-menu" class="dl-menuwrapper">
+						<button class="dl-trigger">Open Menu</button>
+						<ul class="dl-menu">
+							<li>
+								<a href="#">Fashion</a>
+								<ul class="dl-submenu">
+									<li>
+										<a href="#">Men</a>
+										<ul class="dl-submenu">
+											<li><a href="#">Shirts</a></li>
+											<li><a href="#">Jackets</a></li>
+											<li><a href="#">Chinos &amp; Trousers</a></li>
+											<li><a href="#">Jeans</a></li>
+											<li><a href="#">T-Shirts</a></li>
+											<li><a href="#">Underwear</a></li>
+										</ul>
+									</li>
+									<li>
+										<a href="#">Women</a>
+										<ul class="dl-submenu">
+											<li><a href="#">Jackets</a></li>
+											<li><a href="#">Knits</a></li>
+											<li><a href="#">Jeans</a></li>
+											<li><a href="#">Dresses</a></li>
+											<li><a href="#">Blouses</a></li>
+											<li><a href="#">T-Shirts</a></li>
+											<li><a href="#">Underwear</a></li>
+										</ul>
+									</li>
+									<li>
+										<a href="#">Children</a>
+										<ul class="dl-submenu">
+											<li><a href="#">Boys</a></li>
+											<li><a href="#">Girls</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">Electronics</a>
+								<ul class="dl-submenu">
+									<li><a href="#">Camera &amp; Photo</a></li>
+									<li><a href="#">TV &amp; Home Cinema</a></li>
+									<li><a href="#">Phones</a></li>
+									<li><a href="#">PC &amp; Video Games</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">Furniture</a>
+								<ul class="dl-submenu">
+									<li>
+										<a href="#">Living Room</a>
+										<ul class="dl-submenu">
+											<li><a href="#">Sofas &amp; Loveseats</a></li>
+											<li><a href="#">Coffee &amp; Accent Tables</a></li>
+											<li><a href="#">Chairs &amp; Recliners</a></li>
+											<li><a href="#">Bookshelves</a></li>
+										</ul>
+									</li>
+									<li>
+										<a href="#">Bedroom</a>
+										<ul class="dl-submenu">
+											<li>
+												<a href="#">Beds</a>
+												<ul class="dl-submenu">
+													<li><a href="#">Upholstered Beds</a></li>
+													<li><a href="#">Divans</a></li>
+													<li><a href="#">Metal Beds</a></li>
+													<li><a href="#">Storage Beds</a></li>
+													<li><a href="#">Wooden Beds</a></li>
+													<li><a href="#">Children's Beds</a></li>
+												</ul>
+											</li>
+											<li><a href="#">Bedroom Sets</a></li>
+											<li><a href="#">Chests &amp; Dressers</a></li>
+										</ul>
+									</li>
+									<li><a href="#">Home Office</a></li>
+									<li><a href="#">Dining &amp; Bar</a></li>
+									<li><a href="#">Patio</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">Jewelry &amp; Watches</a>
+								<ul class="dl-submenu">
+									<li><a href="#">Fine Jewelry</a></li>
+									<li><a href="#">Fashion Jewelry</a></li>
+									<li><a href="#">Watches</a></li>
+									<li>
+										<a href="#">Wedding Jewelry</a>
+										<ul class="dl-submenu">
+											<li><a href="#">Engagement Rings</a></li>
+											<li><a href="#">Bridal Sets</a></li>
+											<li><a href="#">Women's Wedding Bands</a></li>
+											<li><a href="#">Men's Wedding Bands</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div><!-- /dl-menuwrapper -->
+				</div>  
+                
+                <div class="col-md-9 about_us_header">
+                	<h1>Website Design <span>& Development</span></h1>
+                </div>	     
+            </div>
+            <div class="row about_us_sections ">
+            	<div class="col-md-12">
+                	<p style="color:#E5E4E2">A2N is a full-service online marketing and web development firm dedicated to building your brand. With 17 years in the business, hundreds of satisfied clients, and a creative team of accomplished experts, A2N is the solution to develop an amazing website that will deliver remarkable results for your business.
+                    <br><br>
+                    <span style="font-size:25px; font-weight:bold">We’re different—not peculiar different—but particular different. Being particular different means that you—our clients—benefit.</span></p>
+                </div>            
+            </div>
+            </div>	
+            
+     </div>
+            
+     
+         <div class="container-fluid "> 
+                <div class="container">
+                    <div class="row about_us_sections">
+                        <h1 style="text-align:center; margin-bottom:50px">Building Your Brand with the Perfect Site</h1>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h2>SMALL BUSINESS WEBSITES</h2>
+                                <p>Many small business owners don’t have the budget to shell out big bucks for a website. But doing it on your own can seem overwhelming. Now you can get a great website with just the functionality you need, for a fraction of the price.</p>
+                            </div>
+                            <div class="col-md-6">
+                                <h2>LEAD GENERATION WEBSITES</h2>
+                                <p>Let us develop your website into your business’s hardest-working employee. Our lead generation strategy helps to ensure that visitors take action, increasing your conversion rates.</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h2>CMS WEBSITES</h2>
+                                <p>A2N can design a content management system to fit your needs with an adjustable and intuitive site that fits your needs. Updating and tweaking your website on your own will never be more seamless – or more simple.</p>
+                            </div>
+                            <div class="col-md-6">
+                                <h2>MOBILE WEBSITES</h2>
+                                <p>Optimizing your site for mobile is critical when your target audience increasingly finds you on a tablet or smartphone. We’ll code your website to adapt automatically, for an amazing experience regardless of device size.</p>
+                            </div>
+                        </div>             
+                    </div> 	
+                </div> 
+         </div>
+         
+         <div class="container-fluid color_background_2"> 
+                <div class="container">
+                    <div class="row about_us_sections">
+                        <h1 style="text-align:center; margin-bottom:50px">The <strong>A2N</strong> Approach</h1>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <p style="color:#fff; text-align:center">At A2N, we take a much different approach to building the best website designs. First, we take the time to get to know you, your business, and the goals you have for your website. Whether you need to provide basic product information, want to get more leads, sell your products and services online, or improve your branding and recognition in the marketplace, we want to help you turn your site into the vehicle that gets you there!</p>
+                            </div>      
+                        </div>            
+                    </div> 	
+                </div> 
+         </div>
+         
+         <div class="container-fluid"> 
+                <div class="container">
+                    <div class="row about_us_sections">
+                        <div class="row" style="margin-top:30px">
+                            <div class="col-md-7">
+                                <img class="img-responsive" style=" margin-top:100px" src="images/design-and-development-process.png">
+                            </div>
+                            <div class="col-md-1"></div>
+                            <div class="col-md-4">
+                                <img class="img-responsive" src="images/development-work.png">
+                            </div>
+                        </div>            
+                    </div> 	
+                </div> 
+         </div>
+   
+       
+        <footer>
+        	<div class="container">
+            	<div class="row">
+                	<div class="col-md-3">
+                    	<h2>IT Craft by the Numbers</h2>
+                        <ul>
+                        	<li>We provide outsourced software development services to over 50 active clients from 21 countries</li>
+                            <li>238 employees and growing</li>
+                            <li>15 successful years in business and looking forward to many more</li>
+                        </ul>
+                        
+                        <p>
+                        	<a href="#"><i class="fa fa-google-plus-square footer-social-icon" aria-hidden="true"></i></a>&nbsp;
+                         	<a href="#"><i class="fa fa-facebook-square footer-social-icon" aria-hidden="true"></i></a>&nbsp;
+                         	<a href="#"><i class="fa fa-linkedin-square footer-social-icon" aria-hidden="true"></i></a>&nbsp;
+                        </p>
+                    </div>
+                    <div class="col-md-3">
+                    	<h2>We are globally present</h2>
+                        <ul>
+                        	<p>USA: +1-212-726-4422<br>
+                            Canada: +1-519-362-8426<br>
+                            Germany: +49-1575-366-91-79</p>
+                            <p>For more information email us <a href="#">site@itechcraft.com</a></p>
+                            <p>Contact us! The next success story will be yours.</p>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                    	<h2>We provide software programming services to</h2>
+                        <ul>
+                        	<a href="#"><li>Startups</li></a>
+                            <a href="#"><li>Digital Agencies</li></a>
+                            <a href="#"><li>Indoor Positioning</li></a>
+                            <a href="#"><li>Logistics and Supply Chain</li></a>
+                            <a href="#"><li>Outdoor Navigation</li></a>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                    	<h2>Expertise</h2>
+                        <p>iOS / Android / HTML5
+/PHP/ ASP.Net / C# / J2ME
+Microsoft Dynamics CRM / Zend / Titanium / PhoneGap /
+Magento / WordPress / LAMP / Xamarin / AngularJS / NodeJS / Development for iBeacons, Drones, AppleTV,
+…long lists are boring, so feel
+free to send us a request to
+site@itechcraft.com</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="js/jquery.dlmenu.js"></script>
+		<script>
+			$(function() {
+				$( '#dl-menu' ).dlmenu({
+					animationClasses : { classin : 'dl-animate-in-3', classout : 'dl-animate-out-3' }
+				});
+			});
+		</script>
+	</body>
+</html>
